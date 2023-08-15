bee2e23e17b2d785c969cef1eba50bd94ebc44b5
ramed12 <ramedmendes@gmail.com>
2023-08-15 17:57:06 -0300

    Memoteca-master

---
+++ src/app/componentes/pensamentos/pensamento.ts
@@ -0,0 +1,9 @@
+﻿export interface Pensamento
+ {
+  id?: number
+  conteudo: string
+  autoria: string
+  modelo: string
+ }
+
+ // como se fosse um contrato que precisa ser respeitado por ambas as classes
